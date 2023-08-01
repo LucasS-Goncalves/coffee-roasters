fbe01b2723f1628874cfcb2332688de0fd112918
Lucas <lucasgoncalves08@live.com>
2023-08-01 12:34:18 -0300

    finishing howItWorks section layout for mobile

--- src/app/components/home-page/home-page.component.ts
+++ src/app/components/home-page/home-page.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home-page',
   templateUrl: './home-page.component.html',
-  styleUrls: ['./home-page.component.css']
+  styleUrls: ['./home-page.component.css', './styles/work-steps.component.css']
 })
 export class HomePageComponent {
 
@@ -49,5 +49,23 @@ export class HomePageComponent {
       description: 'We cover the cost and coffee is delivered fast. Peak freshness: guaranteed.',
       alt: 'Truck icon'
     }
+  ]
+
+  workSteps = [
+    {
+      number: '01',
+      stepName: 'Pick your coffee',
+      stepDescription: 'Select from our evolving range of artisan coffees. Our beans are ethically sourced and we pay fair prices for them. There are new coffees in all profiles every month for you to try out.',
+    },
+    {
+      number: '02',
+      stepName: 'Choose the frequency',
+      stepDescription: 'Customize your order frequency, quantity, even your roast style and grind type. Pause, skip or cancel your subscription with no commitment through our online portal.',
+    },
+    {
+      number: '03',
+      stepName: 'Receive and enjoy!',
+      stepDescription: 'We ship your package within 48 hours, freshly roasted. Sit back and enjoy award-winning world-class coffees curated to provide a distinct tasting experience.',
+    },
   ]
 }
