e552ea7f49d6c9dfe5a4c334ff18c7152146bfec
Lucas <lucasgoncalves08@live.com>
2023-08-02 13:58:09 -0300

    finishing aboutUs section layout for mobiles

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: [
+    './about.component.css',
+    './styles/about-us-section.component.css'
+  ]
 })
 export class AboutComponent {
 
