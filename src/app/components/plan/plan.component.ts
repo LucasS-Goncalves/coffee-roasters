8889b93c5ee4bf7b5562a10727dddbf7d97566c1
Lucas <lucasgoncalves08@live.com>
2023-08-03 16:29:39 -0300

    finishing main-image section for tablets

--- src/app/components/plan/plan.component.ts
+++ src/app/components/plan/plan.component.ts
@@ -3,7 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-plan',
   templateUrl: './plan.component.html',
-  styleUrls: ['./plan.component.css']
+  styleUrls: [
+    './plan.component.css',
+    './styles/main-img-section.component.css'
+  ]
 })
 export class PlanComponent {
 
